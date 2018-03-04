e83e915948b3e0421bca17e3233771c18b8bcf27
Prasanna <prasannasv@users.noreply.github.com>
2018-03-03 16:51:48 -0800

    Prevent duplicate payment by associating an invoice num with each paypal transaction (#2)
    * Generate an unique invoice number for each child program combination
    
    * Make it a sha256 to reduce chances of collision

--- src/main/java/org/ishausa/registration/cp/payment/PaymentInfo.java
+++ src/main/java/org/ishausa/registration/cp/payment/PaymentInfo.java
@@ -6,14 +6,20 @@ import com.google.common.base.MoreObjects;
  * Created by Prasanna Venkat on 3/4/2017.
  */
 public class PaymentInfo {
+    private final String invoiceId;
     private final int amount;
     private final String custom;
     private final String description;
 
-    public PaymentInfo(final int amount, final String custom, final String description) {
+    public PaymentInfo(final String invoiceId, final int amount, final String custom, final String description) {
+        this.invoiceId = invoiceId;
         this.amount = amount;
         this.custom = custom;
         this.description = description;
+    }
+
+    public String getInvoiceId() {
+        return invoiceId;
     }
 
     public int getAmount() {
